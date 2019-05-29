5d5b8623c149ab4bccddce89cd5e1f69019d1d58
Cosimo Pasquale RICCHIUTO <cosimo-pasquale.ricchiuto@amadeus.com>
2019-05-29 17:46:06 +0200

    update index with accommodations

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const fetch = require("node-fetch");
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
-    cart(id: String): Cart
+    cart(id: String): CartReply
+    airOffers(
+      originLocationCode: String
+      destinationLocationCode: String
+      departureDateTime: String
+      returnDateTime: String
+      commercialFareFamily: String
+      travelerTypes: String
+    ): AirOfferList
   }
 
   type Mutation {
     createCart: Cart
+    addAirOffer(cartId: String, airOfferInput: AirOfferInput): [AirOffer]
     addTravelers(cartId: String, travelers: [TravelerInput]): [Traveler]
     addContacts(cartId: String, contacts: ContactInput): [IContact]
     addFrequentFlyers(
@@ -17,11 +26,131 @@ const typeDefs = gql`
     ): [FrequentFlyerCard]
   }
 
+  type AirOfferList {
+    airOffers: [AirOffer]
+  }
+
+  type AirOffer {
+    id: String
+    offerItems: [OfferItem]
+  }
+
+  type OfferItem {
+    id: String
+    prices: AirPricingRecords
+    air: AirItem
+  }
+
+  type AirPricingRecords {
+    unitPrices: [UnitPrice]
+    totalPrices: [Price]
+  }
+
+  type UnitPrice {
+    travelerIds: [String]
+    prices: [Price]
+  }
+  type Price {
+    base: Int
+    total: Int
+    currencyCode: String
+    totalTaxes: Int
+  }
+
+  type AirItem {
+    bounds: [Bound]
+    prices: AirPricingRecords
+    travelerIds: [String]
+    fareFamilyCode: String
+    fareInfos: [FareInfo]
+  }
+
+  type Bound {
+    originLocationCode: String
+    destinationLocationCode: String
+    prices: AirPricingRecords
+    flights: [Flight]
+    fareFamilyCode: String
+    duration: Int
+  }
+
+  type Flight {
+    id: String
+    cabin: String
+    bookingClass: String
+    statusCode: String
+    quota: Int
+  }
+
+  type FareInfo {
+    fareType: String
+    fareClass: String
+    pricedPassengerTypeCodes: [String]
+    travelerIds: [String]
+    ticketDesignator: String
+    flightIds: [String]
+  }
+  type CartReply {
+    data: Cart
+    dictionaries: CartDictionaries
+  }
+
   type Cart {
     id: String
     travelers: [Traveler]
     contacts: [IContact]
     frequentFlyerCards: [FrequentFlyerCard]
+    airOffers: [AirOffer]
+    accommodations: [Accommodation]
+  }
+  
+  type CartDictionaries {
+    location: LocationDic
+    flight: FlightDic
+  }
+  
+  type FlightDic {
+    marketingAirlineCode: String
+    operatingAirlineCode: String
+    operatingAirlineName: String
+    marketingFlightNumber: String
+    departure: FlightEndPoint
+    arrival: FlightEndPoint
+    aircraftCode: String
+    duration: Int
+    secureFlightIndicator: Boolean
+  }
+
+  type FlightEndPoint {
+    locationCode: String
+    dateTime: String
+    terminal: String
+  }
+  
+  type Accommodation {
+    code: String
+    name: String
+    destinationName: String
+    latitude: String
+    longitude: String
+    rooms: [Room]
+    minRate: String
+    maxRate: String
+    currencyCode: String
+  }
+  
+  type Room {
+    code: String
+    name: String
+    rates: RoomRate
+  }
+
+  type RoomRate {
+    rateKey: String
+    rooms: Int
+    adults: Int
+    children: Int
+    sellingRate: String
   }
 
   type Traveler {
@@ -36,6 +165,10 @@ const typeDefs = gql`
     names: [IdentityInput]
     dateOfBirth: Date
     tid: String
+  }
+
+  input AirOfferInput {
+    airOfferId: String
   }
 
   type Identity {
@@ -176,12 +309,29 @@ const headers = {
   "Content-Type": "application/json"
 };
 
+const baseUrl = "https://proxy.digitalforairlines.com/v2";
+
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
     cart: (_, { id }) => {
+      return fetch(baseUrl + `/shopping/carts/${id}`, { headers })
+        .then(res => res.json());
+    },
+    airOffers: (
+      _,
+      {
+        originLocationCode,
+        destinationLocationCode,
+        departureDateTime,
+        returnDateTime,
+        commercialFareFamily,
+        travelerTypes
+      }
+    ) => {
       return fetch(
-        `https://proxy.digitalforairlines.com/v2/shopping/carts/${id}`,
+        baseUrl +
+          `/search/air-offers?originLocationCode=${originLocationCode}&destinationLocationCode=${destinationLocationCode}&departureDateTime=${departureDateTime}&returnDateTime=${returnDateTime}&commercialFareFamilies=${commercialFareFamily}&travelers=${travelerTypes}`,
         { headers }
       )
         .then(res => res.json())
@@ -190,22 +340,36 @@ const resolvers = {
   },
   Mutation: {
     createCart: () => {
-      return fetch("https://proxy.digitalforairlines.com/v2/shopping/carts", {
+      return fetch(baseUrl + "/shopping/carts", {
         method: "POST",
         headers
       })
         .then(res => res.json())
         .then(data => data && data.data);
     },
+    addAirOffer: (_, { cartId, airOfferInput }) => {
+      return fetch(baseUrl + `/shopping/carts/${cartId}/air-offers`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(airOfferInput)
+      })
+        .then(res => res.json())
+        .then(res => {
+          console.log(res);
+          return res;
+        })
+        .then(data => data && data.data)
+        .catch(error => {
+          console.error(error);
+          return error;
+        });
+    },
     addTravelers: (_, { cartId, travelers }) => {
-      return fetch(
-        `https://proxy.digitalforairlines.com/v2/shopping/carts/${cartId}/travelers`,
-        {
-          method: "POST",
-          headers,
-          body: JSON.stringify(travelers)
-        }
-      )
+      return fetch(baseUrl + `/shopping/carts/${cartId}/travelers`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(travelers)
+      })
         .then(res => res.json())
         .then(data => data && data.data)
         .catch(error => {
@@ -214,17 +378,14 @@ const resolvers = {
         });
     },
     addContacts: (_, { cartId, contacts }) => {
-      return fetch(
-        `http://proxy.digitalforairlines.com/v2/shopping/carts/${cartId}/contacts`,
-        {
-          method: "POST",
-          headers,
-          body: JSON.stringify([
-            ...(contacts.emails || []),
-            ...(contacts.phones || [])
-          ])
-        }
-      )
+      return fetch(baseUrl + `/shopping/carts/${cartId}/contacts`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify([
+          ...(contacts.emails || []),
+          ...(contacts.phones || [])
+        ])
+      })
         .then(res => res.json())
         .then(data => data && data.data)
         .catch(error => {
@@ -233,14 +394,11 @@ const resolvers = {
         });
     },
     addFrequentFlyers: (_, { cartId, frequentFlyerCards }) => {
-      return fetch(
-        `http://proxy.digitalforairlines.com/v2/shopping/carts/${cartId}/frequent-flyer-cards`,
-        {
-          method: "POST",
-          headers,
-          body: JSON.stringify(frequentFlyerCards)
-        }
-      )
+      return fetch(baseUrl + `/shopping/carts/${cartId}/frequent-flyer-cards`, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(frequentFlyerCards)
+      })
         .then(res => res.json())
         .then(data => {
           console.log(data);
