846d722b14e75f8cc865dc71dda56a0880bb40e6
Cosimo Pasquale RICCHIUTO <cosimo-pasquale.ricchiuto@amadeus.com>
2019-06-04 16:19:41 +0200

    updates for hotel integration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
 const fetch = require("node-fetch");
+const SHA256 = require("crypto-js/sha256");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -368,7 +369,7 @@ const resolvers = {
         "Api-key": apiKeyApiTude,
         "X-Signature": signature,
         Accept: "application/json",
-        "Accept-Encoding": "gzip",
+        "Accept-Encoding": "Gzip",
         "Content-Type": "application/json"
       };
       const hotelSearch = {
@@ -390,21 +391,31 @@ const resolvers = {
           unit: "km"
         }
       };
-      return fetch("https://api.test.hotelbeds.com/hotel-api/1.0/hotels", {
+      var apiTudeUrl =
+        "https://4798193b-60a5-4ca7-b064-7625627f013e.mock.pstmn.io";
+      //    "https://api.test.hotelbeds.com";
+      return fetch(apiTudeUrl + "/hotel-api/1.0/hotels", {
         method: "POST",
         headersApiTude,
         body: JSON.stringify(hotelSearch)
       })
+        .then(res => res.json())
         .then(res => {
-          return res.text();
+          console.log(res);
+
+          return res;
+        });
+      /*
+        .then(checkStatus)
+        .then(res => {
+          return res.json();
         })
-        .then(data => {
-          console.log("data: " + JSON.stringify(data));
-        })
+        .then(data => data && data.hotels)
         .catch(error => {
           console.error(error);
           return error;
         });
+        */
     },
     airOffers: (
       _,
@@ -458,14 +469,34 @@ const resolvers = {
               );
               var numberOfNights = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-              var targetLocation = myFlights[0].arrival.locationCode;
+              var targetLocation = {
+                iataCode: myFlights[0].arrival.locationCode
+              };
 
-              console.log(
-                targetLocation +
-                  ": targetLocation " +
-                  numberOfNights +
-                  " nights"
-              );
+              fetch(
+                `http://nano.aviasales.ru/places_en?term=` +
+                  targetLocation.iataCode,
+                { headers }
+              )
+                .then(locationRes => locationRes.json())
+                .then(data => {
+                  targetLocation.latitude = data[0].coordinates[0];
+                  targetLocation.longitude = data[0].coordinates[1];
+
+                  console.log(
+                    JSON.stringify(myFlights) +
+                      ": flights -" +
+                      JSON.stringify(targetLocation) +
+                      "targetLocation" +
+                      numberOfNights +
+                      " nights"
+                  );
+                })
+                .catch(error => {
+                  console.error(error);
+                  return error;
+                });
+
               /* 
               var myExt = [];
               myExt[0] = {
@@ -507,7 +538,7 @@ const resolvers = {
         });
     },
     addExtensions: (_, { cartId, extensions }) => {
-      return fetch(baseUrl + `/shopping/carts/${cartId}/extensions`, {
+      fetch(baseUrl + `/shopping/carts/${cartId}/extensions`, {
         method: "POST",
         headers,
         body: JSON.stringify(extensions)
@@ -569,111 +600,32 @@ server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
 
+const checkStatus = function(res) {
+  if (res.ok) {
+    // res.status >= 200 && res.status < 300
+    console.log(res.status);
+  } else {
+    console.log(res.statusText);
+  }
+  return res;
+};
+const performAPICall = function(url, method, headers, body) {
+  fetch(url, {
+    method: method,
+    headers,
+    body: JSON.stringify(body)
+  })
+    .then(res => res.json())
+    .then(data => data && data.data)
+    .catch(error => {
+      console.error(error);
+      return error;
+    });
+};
+
 const buildSignature = function(apiKey, secret) {
   var d = new Date();
   var n = d.getTime();
   var datetime = Math.floor(n / 1000);
-  return sha256(apiKey + secret + datetime);
-};
-
-const sha256 = function(ascii) {
-  function rightRotate(value, amount) {
-    return (value >>> amount) | (value << (32 - amount));
-  }
-
-  var mathPow = Math.pow;
-  var maxWord = mathPow(2, 32);
-  var lengthProperty = "length";
-  var i, j; // Used as a counter across the whole file
-  var result = "";
-
-  var words = [];
-  var asciiBitLength = ascii[lengthProperty] * 8;
-
-  //* caching results is optional - remove/add slash from front of this line to toggle
-  // Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
-  // (we actually calculate the first 64, but extra values are just ignored)
-  var hash = (sha256.h = sha256.h || []);
-  // Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
-  var k = (sha256.k = sha256.k || []);
-  var primeCounter = k[lengthProperty];
-  /*/
-	var hash = [], k = [];
-	var primeCounter = 0;
-	//*/
-
-  var isComposite = {};
-  for (var candidate = 2; primeCounter < 64; candidate++) {
-    if (!isComposite[candidate]) {
-      for (i = 0; i < 313; i += candidate) {
-        isComposite[i] = candidate;
-      }
-      hash[primeCounter] = (mathPow(candidate, 0.5) * maxWord) | 0;
-      k[primeCounter++] = (mathPow(candidate, 1 / 3) * maxWord) | 0;
-    }
-  }
-
-  ascii += "\x80"; // Append Ƈ' bit (plus zero padding)
-  while ((ascii[lengthProperty] % 64) - 56) ascii += "\x00"; // More zero padding
-  for (i = 0; i < ascii[lengthProperty]; i++) {
-    j = ascii.charCodeAt(i);
-    if (j >> 8) return; // ASCII check: only accept characters in range 0-255
-    words[i >> 2] |= j << (((3 - i) % 4) * 8);
-  }
-  words[words[lengthProperty]] = (asciiBitLength / maxWord) | 0;
-  words[words[lengthProperty]] = asciiBitLength;
-
-  // process each chunk
-  for (j = 0; j < words[lengthProperty]; ) {
-    var w = words.slice(j, (j += 16)); // The message is expanded into 64 words as part of the iteration
-    var oldHash = hash;
-    // This is now the undefinedworking hash", often labelled as variables a...g
-    // (we have to truncate as well, otherwise extra entries at the end accumulate
-    hash = hash.slice(0, 8);
-
-    for (i = 0; i < 64; i++) {
-      var i2 = i + j;
-      // Expand the message into 64 words
-      // Used below if
-      var w15 = w[i - 15],
-        w2 = w[i - 2];
-
-      // Iterate
-      var a = hash[0],
-        e = hash[4];
-      var temp1 =
-        hash[7] +
-        (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) + // S1
-        ((e & hash[5]) ^ (~e & hash[6])) + // ch
-        k[i] +
-        // Expand the message schedule if needed
-        (w[i] =
-          i < 16
-            ? w[i]
-            : (w[i - 16] +
-              (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15 >>> 3)) + // s0
-                w[i - 7] +
-                (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2 >>> 10))) | // s1
-              0);
-      // This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
-      var temp2 =
-        (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) + // S0
-        ((a & hash[1]) ^ (a & hash[2]) ^ (hash[1] & hash[2])); // maj
-
-      hash = [(temp1 + temp2) | 0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
-      hash[4] = (hash[4] + temp1) | 0;
-    }
-
-    for (i = 0; i < 8; i++) {
-      hash[i] = (hash[i] + oldHash[i]) | 0;
-    }
-  }
-
-  for (i = 0; i < 8; i++) {
-    for (j = 3; j + 1; j--) {
-      var b = (hash[i] >> (j * 8)) & 255;
-      result += (b < 16 ? 0 : "") + b.toString(16);
-    }
-  }
-  return result;
+  return SHA256(apiKey + secret + datetime);
 };
